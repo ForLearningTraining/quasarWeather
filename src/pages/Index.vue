36b8c7452ff364215c6606d2c4d25100d197252d
CarlsonLazorus <34118180+CarlsonLazorus@users.noreply.github.com>
2021-09-10 13:01:26 +0800

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-page class="flex column">
+    <div class="col q-pt-lg q-px-md">
+   <q-input
+         bottom-slots
+         v-model="text"
+         label="Label"
+         counter
+         maxlength="12"
+         :dense="dense">
+        <template v-slot:before>
+          <q-icon name="event" />
+        </template>
+
+        <template v-slot:hint>
+          Field hint
+        </template>
+
+        <template v-slot:append>
+          <q-btn round dense flat icon="add" />
+        </template>
+      </q-input>
+    </div>
+   
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex'
+})
+</script>
+
+<style lang="sass">
+.q-page
+  background: linear-gradient(to bottom, #136a8a, #267871)
+</style>
