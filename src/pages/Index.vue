4b471dc707d1d1d9a3d737a560754ab75f5f59be
CarlsonLazorus <34118180+CarlsonLazorus@users.noreply.github.com>
2021-09-10 15:20:03 +0800

    connect openweather api

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,24 +2,62 @@
   <q-page class="flex column">
     <div class="col q-pt-lg q-px-md">
    <q-input
-         bottom-slots
-         v-model="text"
-         label="Label"
-         counter
-         maxlength="12"
-         :dense="dense">
+         v-model="search"
+         placeholder="Search"
+         dark
+         borderless
+         
+        >
         <template v-slot:before>
-          <q-icon name="event" />
-        </template>
-
-        <template v-slot:hint>
-          Field hint
+         <q-icon
+            @click="getLocation"
+            name="my_location" />
         </template>
 
         <template v-slot:append>
-          <q-btn round dense flat icon="add" />
+          <q-btn round dense flat icon="search" />
         </template>
       </q-input>
+    </div>
+    
+    <template v-if="weatherData">
+    <div class="col text-white text-center">
+      <div class="text-h4 text-weight-light">
+         {{weatherData.name}}
+        
+      </div>
+      <div class="text-h6 text-weight-light">
+         {{weatherData.weather[0].main}} 
+         
+      </div>
+      <div class="text-h1 text-weight-thin q-my-lg relative-position">
+         <span> {{Math.round(weatherData.main.temp)}}</span> 
+       
+        <span class="text-h4 relative-position degree">&deg;C</span>
+      </div>
+    </div>
+    <div class="col text-center">
+      <img :src="`http://openweathermap.org/img/wn/${ weatherData.weather[0].icon }@2x.png`"> 
+    </div>
+    </template>
+
+    <template v-else>
+      <div class="col column text-center text-white">
+        <div class="col text-h2 text-weight-thin">
+          Quasar<br>Weather
+        </div>
+        <q-btn
+          @click="getLocation"
+          class="col"
+          flat>
+          <q-icon left size="3em" name="my_location" />
+          <div>Find my location</div>
+        </q-btn>
+      </div>
+    </template>
+
+    <div class="col skyline">
+
     </div>
    
   </q-page>
@@ -29,11 +67,45 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data() {
+    return{
+        search:'',
+        weatherData: null,
+         lat: null,
+         lon: null,
+         apiUrl: 'https://api.openweathermap.org/data/2.5/weather',
+         apiKey: 'b7d283e58f3b50db47c408b5d5565e08'
+    }
+  },
+  methods: {
+    getLocation(){
+      navigator.geolocation.getCurrentPosition
+      (position => {
+        console.log('position:', position)
+        this.lat = position.coords.latitude
+        this.lon = position.coords.longitude
+        this.getWeatherByCoords()
+      })
+    },
+     getWeatherByCoords(){
+       this.$axios(`${ this.apiUrl }?lat=${ this.lat }&lon=${ this.lon }&appid=${ this.apiKey }&units=metric`).then(response => {
+         this.weatherData = response.data
+       })
+     }
+  }
 })
 </script>
 
 <style lang="sass">
-.q-page
-  background: linear-gradient(to bottom, #136a8a, #267871)
+  .q-page
+    background: linear-gradient(to bottom, #136a8a, #267871)
+  .degree
+    top:-44px
+      
+  .skyline
+    flex: 0 0 100px
+    background: url(../statics/skyline.png)
+    background-size: contain
+    background-position: center bottom
 </style>
